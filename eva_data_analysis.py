a99b01a6cb8a3269c2a769766106589e16e0999b
Aman Goel <aman.goel185@gmail.com>
2026-05-08 12:44:55 +0100

    feat: add duration by astronaut function (#6)

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -4,17 +4,19 @@ import sys
 import re
 
 
-def main(input_file, output_file, graph_file):
+def main(input_file, output_file, duration_by_astronaut_output_file, graph_file):
     print("--START--")
 
     # Read the data from JSON file
     eva_data = read_json_to_dataframe(input_file)
 
-    # Calculate and add crew size to data
-    eva_data = add_crew_size_column(eva_data)
-
     # Convert and export data to CSV file
     write_dataframe_to_csv(eva_data, output_file)
+
+    # Calculate summary table for total EVA per astronaut
+    duration_by_astronaut_df = summary_duration_by_astronaut(eva_data)
+    # Save summary duration data by each astronaut to CSV file
+    write_dataframe_to_csv(duration_by_astronaut_df, duration_by_astronaut_output_file)
 
     # Sort dataframe by date ready to be plotted (date values are on x-axis)
     eva_data.sort_values('date', inplace=True)
@@ -153,6 +155,25 @@ def add_crew_size_column(df):
     return df_copy
 
 
+def summary_duration_by_astronaut(df):
+    """
+    Summarise the duration data by each astronaut and saves resulting table to a CSV file
+
+    Args: 
+        df (pd.DataFrame): Input dataframe to be summarised
+
+    
+    Returns:
+        sum_by_astro (pd.DataFrame): Data frame with a row for each astronaut and a summarised column 
+    """
+    print(f'Calculating summary of total EVA time by astronaut')
+    subset = df.loc[:,['crew', 'duration']] # subset to work with only relevant columns
+    subset = add_duration_hours(subset) # need duration_hours for easier calcs
+    subset = subset.drop('duration', axis=1) # dropping the extra 'duration' column as it contains string values not suitable for calulations
+    subset = subset.groupby('crew').sum() 
+    return subset
+
+
 if __name__ == "__main__":
 
     if len(sys.argv) < 3:
@@ -165,5 +186,6 @@ if __name__ == "__main__":
         print('Using custom input and output filenames')
 
     graph_file = 'results/cumulative_eva_graph.png'
+    duration_by_astronaut_output_file = 'results/duration_by_astronaut.csv'
 
-    main(input_file, output_file, graph_file)
+    main(input_file, output_file, duration_by_astronaut_output_file, graph_file)
